38105fad0dc93c222efbf9ff7858fa1acf823b66
sourabhnoob <151834993+sourabhnoob@users.noreply.github.com>
2024-05-18 02:10:23 +0530

    Initial commit

---
+++ src/components/Common/Header/index.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "./styles.css";
+import AnchorTemporaryDrawer from "./drawer";
+
+function Header() {
+  return (
+    <div className="navbar">
+      <h1 className="logo">
+        CryptoTracker <span style={{ color: "var(--blue)" }}></span>
+      </h1>
+
+      <div className="links">
+        <a href="/">
+          <p className="link">Home</p>
+        </a>
+        <a href="/">
+          <p className="link">Compare</p>
+        </a>
+        <a href="/">
+          <p className="link">Watchlist</p>
+        </a>
+      </div>
+      <div className="mobile-drawer">
+        <AnchorTemporaryDrawer />
+      </div>
+    </div>
+  );
+}
+
+export default Header;
