e683dec472197fc0730449dc8915d372b5394466
sourabhnoob <151834993+sourabhnoob@users.noreply.github.com>
2024-05-19 00:23:44 +0530

    3. Menu fixed

--- src/components/Common/Header/drawer.jsx
+++ src/components/Common/Header/drawer.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import Drawer from "@mui/material/Drawer";
-import Button from "@mui/material/Button";
+import IconButton from "@mui/material/Button";
 import MenuRoundedIcon from "@mui/icons-material/MenuRounded";
 
 export default function AnchorTemporaryDrawer() {
